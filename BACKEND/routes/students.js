aaf42d2b2bea8966edbcda2528f99ec52901782a
sonal-uchiru <sonal123326@gmail.com>
2021-08-23 00:42:11 +0530

    change

--- BACKEND/routes/students.js
+++ BACKEND/routes/students.js
@@ -123,7 +123,7 @@ router.post("/login", (req, res) => {
       return res.status(400).json({ msg: "user does not exists" });
 
     if (!(age == studentsss.age)) {
-      return res.status(400).json({ msg: "invalid credentials" });
+      return res.status(400).json({ msg: "Invalid credentials" });
     }
     res.json({
       studentsss: {
