afe3f7eca63458a0f767d47ef902d0d2b5efa602
sonal-uchiru <sonal123326@gmail.com>
2021-08-23 00:40:33 +0530

    intend

--- frontend/src/Components/AddStudent.js
+++ frontend/src/Components/AddStudent.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+export default function AddStudent(props) {
+  let [name, setName] = useState("");
+  let [age, setAge] = useState("");
+  let [gender, setGender] = useState("");
 
-export default function AddStudent(props){
-
-  let [name,setName] = useState("");
-  let [age,setAge] = useState("");
-  let [gender,setGender] = useState("");
-
-  function sendData(e){
+  function sendData(e) {
     // alert("d0");
     e.preventDefault();
 
@@ -16,7 +14,7 @@ export default function AddStudent(props){
       name,
       age,
       gender,
-    }
+    };
 
     // const newStudent2 = {
     //   name : "s",
@@ -25,59 +23,72 @@ export default function AddStudent(props){
     // }
     console.log(newStudent);
     //document.write("newStudent");
-    axios.post("http://localhost:8070/student/add",newStudent).then(()=>{
-      //alert("Student Added");
-      setName(" ");
-      setAge(" ");
-      setGender(" ");
-      props.history.push("/Home");
-      document.getElementById("txt").innerHTML = "Student Added Successfully!";
-      
-    }).catch((err) =>{
-      alert(err)
-    })
+    axios
+      .post("http://localhost:8070/student/add", newStudent)
+      .then(() => {
+        //alert("Student Added");
+        setName(" ");
+        setAge(" ");
+        setGender(" ");
+        props.history.push("/Home");
+        document.getElementById("txt").innerHTML =
+          "Student Added Successfully!";
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }
 
+  return (
+    <div className="container">
+      <form onSubmit={sendData}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Student Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
 
- return(
-   <div className = "container">
-<form onSubmit = {sendData}> 
-  <div className="mb-3">
-    <label htmlFor="name" className="form-label">Student Name</label>
-    <input type="text" className="form-control" id="name"
-     onChange= {
-      (e)=>{
-        setName(e.target.value);
-      }
-    }/>
-  </div>
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">
+            Student Age
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="age"
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="gender" className="form-label">
+            Student Gender
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="gender"
+            onChange={(e) => {
+              setGender(e.target.value);
+            }}
+          />
+        </div>
 
-  <div className="mb-3">
-    <label htmlFor="age" className ="form-label">Student Age</label>
-    <input type="text" className ="form-control" id="age" 
-    onChange= {
-      (e)=>{
-        setAge(e.target.value);
-      }
-    }/>
-  </div>
-  <div className="mb-3">
-    <label htmlFor ="gender" className ="form-label">Student Gender</label>
-    <input type ="text" className ="form-control" id = "gender"
-    onChange= {
-      (e)=>{
-        setGender(e.target.value);
-      }
-    } />
-  </div>
- 
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-   </div>
-  
-
- )
-
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 // export default AddStudent;
