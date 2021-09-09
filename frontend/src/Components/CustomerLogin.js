590ca01a90ebac44ab04f2c66bc218a941a8af2e
Heshishivani <heshishivani@gmail.com>
2021-09-09 22:03:14 +0530

    New Update in Seller Profile

---
+++ frontend/src/Components/CustomerLogin.js
@@ -0,0 +1,127 @@
+
+import axios from "axios";
+import React, {useState} from "react";
+
+
+export default function CustomerLogin(){
+
+    let [username,setUsername] = useState("");
+    let [password,setPassword] = useState("");
+    let [errorMsg,setErrorMsg] = useState("");
+   
+
+    function loginUser(e){
+
+        e.preventDefault();
+
+        const loginCredentials = {
+            username,
+            password,
+          }
+
+          axios.post("http://localhost:8070/Customer/loginCustomer", loginCredentials).then((res)=>{
+
+           // setCustomer(res.data.customerLogin);
+          localStorage.setItem('CustomerID', res.data.customerLogin._id);
+
+             // sessionStorage.setItem('userID',"sss");
+
+            //alert("Customer loggin Successfully!");
+            console.log("logging success");
+            console.log(res.data);
+
+          }).catch((err) =>{
+
+            //alert(err);
+          console.log(err.response.data);
+          alert(err.response.data.error);
+          setErrorMsg(err.response.data.error);
+        
+
+        })
+    }
+
+
+    return(
+
+        <div className = "CustomerLogin">
+        <div className="container">
+        <div className="myCard">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="myLeftCtn"> 
+                        <form className="myForm" onSubmit = {loginUser}>
+                            <header>Sign In</header>
+                            
+							<div className="form-group">
+                                <h2>{errorMsg}</h2>
+							<label htmlFor="username"><b>Username</b></label><br/>
+                                <i className="fas fa-user"></i>
+                                <input className="myInput" type="text" placeholder="Username" id="username" 
+                                
+                                onChange= {
+                                    (e)=>{
+                                      setUsername(e.target.value);
+                                    } }
+                                
+                                    required/> 
+                            </div>
+
+    
+
+                            <div className="form-group">
+							<label htmlFor="password"><b>Password</b></label><br/>
+                                <i className="fas fa-lock"></i>
+                                <input className="myInput" type="password" id="password" placeholder="Password" 
+                                
+                                onChange= {
+                                    (e)=>{
+                                      setPassword(e.target.value);
+                                    } }
+                                
+                                
+                                    required /> 
+                            </div>
+
+                            <br/>
+
+                            <input type="submit" className="butt" value="Sign In"/>
+							
+							<br/><br/>
+							
+							<div className="form-row">
+								<div className="col">
+                                <input type="checkbox" id="che_remember" value="Remember me"/>
+								<label className="remember"> Remember me </label>
+								</div>
+						
+								<div className="col">
+									<p className ="fp"> <a href="">Forgot Password?</a></p>
+								</div>
+							</div>
+							
+							
+                            
+                        </form>
+                    </div>
+                </div> 
+                <div className="col-md-6">
+                    <div className="myRightCtn">
+                            <div className="box"><header><b>Welcome to login</b></header>
+                            
+                            <h4 className ="acc" >Don't have an account?</h4><br/>
+							
+                                <input type="button" className="butt_out" value="Sign Up"/>
+                            </div>
+                                
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+
+    )
+
+
+}
--- frontend/src/Components/SellerProfile.js
+++ frontend/src/Components/SellerProfile.js
@@ -1,118 +1,149 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-import "../css/sellerprofile.css";
+import "../Css/sellerprofile.css";
 
+export default function SellerProfile(props) {
+  return (
+    <div className="height-100 bg-light">
+      <br />
+      <br />
+      <div class="container">
+        <div class="main-body">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <br></br>
+                    <img
+                       src = {require('../images/flight-logo.png').default} 
+                      alt="Admin"
+                      class="rounded-circle p-1 bg-black"
+                      width="175"
+                    />
+                    <br></br>
+                  </div>
 
-export default function SellerProfile(props){ 
+				  <div className = "card border border-dark rounded ">
+					  <br/>
+					  <div className = "card-body">
+                  <ul class="list-group list-group-flush">
+                    <div class="shop">
+                      <h4 class="mb-0" style = {{textAlign : "center"}}>AMAR ELECTRONICS</h4>
+                    </div>
+                    <br></br>
 
-    return(
-        <div class="container">
-		<div class="main-body">
-			<div class="row">
-				<div class="col-lg-4">
-					<div class="card">
-						<div class="card-body">
-							<div class="d-flex flex-column align-items-center text-center"><br></br>
-								<img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Admin" class="rounded-circle p-1 bg-primary" width="175"/>
-								<br></br>
-							</div>
-                           
-							<hr class="my-4"></hr>
-							<ul class="list-group list-group-flush">
-                                    <div class="shop">
-									<h6 class="mb-0">AMAR ELECTRONICS</h6></div>
-									<br></br>
-								
-                                    <div class="shop">
-									<h6 class="mb-0">0772156489</h6></div>
-									<br></br>
-							
-                                    <div class="shop">
-									<h6 class="mb-0">amarele89@gmail.com</h6></div>
-									<br></br>
-							
-								
-                                <br></br><br></br><br></br><br></br><br></br>
-                                <div class="mt-3">
-									<button class="btn btn-primary btn-block button-shape">REQUEST DELETE</button>
-								</div>
-							</ul>
-						</div>
-					</div>
-				</div>
+                    <div class="shop">
+                      <h6 class="mb-0" style = {{textAlign : "center"}} >0772156489</h6>
+                    </div>
+                    <br></br>
 
-				<div class="col-lg-8">
-					<div class="card">
-						<div class="card-body">  
+                    <div class="shop">
+                      <h6 class="mb-0" style = {{textAlign : "center"}}>amarele89@gmail.com</h6>
+                    </div>
+                    <br></br>
 
-                            <div class="row mb-3">
-                                <div class="col-sm-9 text-secondary"> 
-									<input type="text" class="form-control" value="OWNER'S NAME" readOnly/>
-                                </div> 
-							</div> 
-							 
-                            <div class="row mb-3">
-								 <div class="col-sm-9 text-secondary"> 
-									<input type="text" class="form-control" value="COMPANY NAME" readOnly/>
-							    </div> 
-                            </div>
-
-							<div class="row mb-3">
-								<div class="col-sm-9 text-secondary">
-									<input type="text" class="form-control" value="CONTACT NUMBER" readOnly/>
-								</div>
-							</div>
-
-							<div class="row mb-3">
-
-								<div class="col-sm-9 text-secondary">
-									<input type="text" class="form-control" value="EMAIL ADDRESS" readOnly/>
-								</div>
-							</div>
-						</div>
-					</div>
-                    <div class="row">
-						<div class="col-sm-12">
-							<div class="card">
-								<div class="card-body">
-                                <div class="row mb-3">
-
-								<div class="col-sm-9 text-secondary">
-									<textarea class="form-control" value="DESCRIPTION" readOnly/>
-								</div>
-							</div>
-							<div class="row mb-3">
-
-								<div class="col-sm-9 text-secondary">
-									<input type="text" class="form-control" value="ESTABLISHED YEAR" readOnly/>
-								</div>
-							</div>
-							<div class="row mb-3">
-
-								<div class="col-sm-9 text-secondary">
-									<input type="text" class="form-control" value="PHYSICAL ADDRESS" readOnly/>
-								</div>
-							</div>
-
-							<div class="row">
-								<div class="col-sm-3"></div>
-								<div class="col-sm-9 text-secondary center">
-									<input type="button" class="btn btn-primary px-5 btn-align" value="EDIT"/>
-								</div>
-							</div>
-					
-								</div>
-							</div>
-						</div>
-                    </div> 
+             
+                
+                    <div class="mt-3">
+                      <button class="btn btn-danger btn-block button-shape">
+                        REQUEST DELETE
+                      </button>
+                    </div>
+                  </ul>
+				  <br/></div></div>
                 </div>
-        </div>       
+              </div>
+            </div>
 
-        </div> 
-        </div> 				
-    
-
- );
-
+            <div class="col-lg-8">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="First name"
+                      />
+                    </div>
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Last name"
+                      />
+                    </div>
+                  </div>
+                  <br />
+                  <br />
+                  <div class="row">
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="First name"
+                      />
+                    </div>
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Last name"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col">
+                          <div class="form-group">
+                            <textarea
+                              class="form-control"
+                              id="exampleFormControlTextarea1"
+                              rows="7"
+                              placeholder="Description"
+                            ></textarea>
+                          </div>
+                        </div>
+                        <div class="col">
+                          <input
+                            type="text"
+                            class="form-control"
+                            placeholder="Last name"
+                          />
+                          <br />
+                          <div className="row">
+                            <div class="form-group">
+                              <textarea
+                                class="form-control"
+                                id="exampleFormControlTextarea1"
+                                rows="4"
+                                placeholder="physical address"
+                              ></textarea>
+                            </div>
+                          </div>
+                        </div>
+						<div className = "container">
+						<div class="float-right">
+						<button type="button" class="btn btn-primary">EDIT</button>
+							<span> </span>
+							</div>
+						</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
