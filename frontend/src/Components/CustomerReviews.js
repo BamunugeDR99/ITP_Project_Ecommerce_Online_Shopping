b0c32dbadaecac83ad6fd019b14cedfb4f704ea9
sonal-uchiru <sonal123326@gmail.com>
2021-09-10 23:56:24 +0530

    review complete

--- frontend/src/Components/CustomerReviews.js
+++ frontend/src/Components/CustomerReviews.js
@@ -1,123 +1,113 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
 import "../CSS/cusreview.css";
 
-export default function CustomerReviews(props){
+export default function CustomerReviews(props) {
+  let [review, setReview] = useState([]);
+  let re = [];
+  const [customertest, setCustomertest] = useState([]);
+  let [customer, setCustomer] = useState([]);
+  let customers = [];
+  let customerName = "";
+  let customerImage = "";
+  let Review = "";
+  let [abc, setabc] = useState([]);
+  let reviewWithCustomer = {
 
-    let [review,setReview] = useState([]);
-	const [customertest,setCustomertest] = useState([]);
-    // const [loads,setLoad] = useState(false);
-    let [customer , setCustomer]= useState([]);
-    let customers = [];
-// 11
-// h ,j
-  useEffect(() =>{
-    function getReview(){
-        axios.get("http://localhost:8070/review/get").then((res) =>
-        {
-            setReview(res.data);
-            const filter=res.data.filter((itemrev)=> itemrev.itemid ==="611f4784c259414e3c405a34"
-            );
-            review=filter
-            console.log(review)
-            for(let i = 0; i < review.length; i++){
-                let customerid = review[i].customerid
-                console.log(customerid);
+    customerName,
+    customerImage,
+    Review
+  
+} 
 
-                axios.get("http://localhost:8070/customertest/get/"+customerid).then((res) =>{
-                    console.log(res.data);
-                    customer=(res.data);
-                    customers.push(customer);
-                    console.log(customers);
-                })
-            .catch((err) =>{
+  let reviewWithCustomers = [];
+
+
+
+  useEffect(() => {
+    function getReview() {
+      axios
+        .get("http://localhost:8070/review/get")
+        .then((res) => {
+          //setReview(res.data);
+          const filter = res.data.filter(
+            (itemrev) => itemrev.itemid === "611f4784c259414e3c405a34"
+          );
+         // review = setReview(filter);
+          re = filter;
+        //   console.log(re.length);
+
+
+          for (let i = 0; i < re.length; i++) {
+            console.log("sdf");
+            let customerid = re[i].customerid;
+            // console.log(customerid);
+      
+            axios
+              .get("http://localhost:8070/customertest/get/" + customerid)
+              .then((res) => {
+                // console.log(res.data);
+      
+                reviewWithCustomer = {
+                    customerName : res.data.name,
+                    customerImage : res.data.cusimage,
+                    Review : re[i].description
+                }    
+      
+                // console.log(reviewWithCustomer);
+                reviewWithCustomers.push(reviewWithCustomer);
+                //console.log(reviewWithCustomers);
+                setabc(reviewWithCustomers);
+              
+              })
+              .catch((err) => {
                 alert(err);
-            })
-                // axios.get(customerid)
-                // setCustomer(res.data);
-                // customers.push(customer);
-        }
+              });
+      
+          }
 
-
-            // console.log(res.data);
-            
-            
-        }).catch((err) =>{
-            alert(err);
         })
+        .catch((err) => {
+          alert(err);
+        });
     }
-   
+
+
+
     getReview();
-
-    
-   
-}, []);
-customers.map((c)=>{
-    console.log(c.name)
-})
-
- return(
-
- <div class="rev">
-    <h1 id="h1"> Customer Reviews</h1>
-    <h5 id="h5">what our customers say about us</h5>
-
-    <div className="row" style={{ padding: '80px' }}>
-
-	{review.map((review,index) =>{
-
-    <h1 id = "txt"></h1>
-        return( 
-
-        <div className="col-sm-3" >
-            <div  className="card" style={{width: '18rem'}}>
-                <div>
-
-                {customers.map((pack)=> {
-                    return(
-                        <div>
-                        <div>
-                            <img src={`../images/${pack.cusimage}`} className="card-img-top"/>
-                        </div>
-                        <div>
-                        
-                        <h5>dfb{pack.name}</h5>
-     
-                    </div>
-                    </div>
-                    )
-                    })
-                    }
-                            
+    // getCustomer();
+  },[]);
 
 
 
+  
 
-                    <br/>
-                    <div className="reviews">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="far fa-star"></i>
-                        </div>
-                </div>
-                <div className="card-body">
-                    <p className="card-text">{review.description}</p>
-                </div>
+  return (
+    <div class="rev">
+      <h1 id="h1"> Customer Reviews</h1>
+      <h5 id="h5">what our customers say about us</h5>
+
+      {abc.map((reviewss) => {
+        return (
+          <div class="card" style={{width: "18rem"}}>
+            <div class="card-body">
+              <h5 class="card-title">{reviewss.customerName}</h5>
+              {/* <img src =  */}
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">
+             {reviewss.Review}
+              </p>
+              <a href="#" class="card-link">
+                Card link
+              </a>
+              <a href="#" class="card-link">
+                Another link
+              </a>
             </div>
-        </div>
-
-        )
-        })}	
-
+          </div>
+        );
+      })}
     </div>
-</div> 
-
-
- )
-
+  );
 }
-
-
