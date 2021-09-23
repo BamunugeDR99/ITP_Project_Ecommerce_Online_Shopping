dececf4dd34b169cc0f3ae56a55799561846649d
sonal-uchiru <sonal123326@gmail.com>
2021-09-23 21:21:24 +0530

    Add to wishlist and add to cart is complete

--- frontend/src/Components/customer_wishlist.js
+++ frontend/src/Components/customer_wishlist.js
@@ -4,7 +4,7 @@ import { ClipLoader } from "react-spinners";
 import { Link } from "react-router-dom";
 
 export default function Customer_wishlist(props) {
-    const [items, setItems] = useState([]);
+    let [items,setItems] = useState([]);
     const [ratings, setRatings] = useState([]);
     let itemIDs = [];
     let itemID = "";
@@ -17,52 +17,66 @@ export default function Customer_wishlist(props) {
     let objectID = "";
 
     let result;
+    let wishlist;
     useEffect(() => {
       async function getItems() {
         //change url
         objectID = localStorage.getItem("CustomerID");
         axios
-          .get("http://localhost:8070/wishlist/getItems")
+          .post("http://localhost:8070/wishlist/getByCustomerID/"+ objectID)
           .then((res) => {
-            console.log(res.data);
-             result = res.data.filter(
-              (post) =>
-                post.CustomerID == objectID 
+           // console.log(res.data);
+
+        
                 
-            );
-            console.log(result);
+            wishlist = res.data.wishlistss;
+
+            console.log(wishlist.Items);
            // setStudents(result);
-           result.Items;
-            console.log(result.items);
+          // result.Items;
+            // console.log(result.items);
             // console.log(itemIDs.length)
+        
+
+            //console.log(res.data);
+
+
+
+
             axios
             .get("http://localhost:8070/items/getItems")
             .then((res) => {
               
               let result2 = res.data;
                 let newItems = [];
-
-              for(let i = 0; i < 1; i++){
-                  for(let j = 0; j < 3; j++){
-                      if(itemIDs[i] == result2[j]._id){
+  
+              for(let i = 0; i < wishlist.Items.length; i++){
+                  for(let j = 0; j < result2.length; j++){
+                      if(wishlist.Items[i] == result2[j]._id){
                         newItems.push(result2[j]);
                       }
                   }
               }
-
+  
               console.log(newItems);
+           
+              setItems(newItems);
+              //items = newItems;
                           
               
             })
             .catch((err) => {
               alert(err);
             });
-
-            //console.log(res.data);
           })
           .catch((err) => {
             alert(err);
           });
+
+
+
+
+       
       }
 
        function displayRating(){
@@ -77,163 +91,162 @@ export default function Customer_wishlist(props) {
           alert(err);
         });
       }
-    //displayRating();
+   // displayRating();
      getItems();
 
     }, []);
 
-//     useEffect(() => {
-//       //displayStatus();
-//      // calculateStarRating(1);
+    useEffect(() => {
+      //displayStatus();
+    // calculateStarRating(1);
 
-//     })
+    })
 
-//     useEffect(() =>{
-//       //displayStarRating();
-//     })
+    useEffect(() =>{
+      //displayStarRating();
+    })
 
-//   function calculateStarRating(id){
-//     let totalNoRatings = 0;
-//     let totalstarforRatingCount = 0;
-//     let starCount = 0;
-//     let average = 0;
+  // function calculateStarRating(id){
+  //   let totalNoRatings = 0;
+  //   let totalstarforRatingCount = 0;
+  //   let starCount = 0;
+  //   let average = 0;
 
-//     for(let i = 0; i < items.length; i++){
+  //   for(let i = 0; i < items.length; i++){
 
-//       totalNoRatings = 0;
-//       totalstarforRatingCount = 0;
-//       starCount = 0;
-//       average = 0;
+  //     totalNoRatings = 0;
+  //     totalstarforRatingCount = 0;
+  //     starCount = 0;
+  //     average = 0;
 
-//       for(let j = 0; j < ratings.length; j++){
-//           if(items[i]._id == ratings[j].itemid){
-//             totalNoRatings++;
-//           }
+  //     for(let j = 0; j < ratings.length; j++){
+  //         if(items[i]._id == ratings[j].itemid){
+  //           totalNoRatings++;
+  //         }
 
-//           if(items[i]._id == ratings[j].itemid){
-//             starCount += parseInt(ratings[j].noofstars);
-//           }
+  //         if(items[i]._id == ratings[j].itemid){
+  //           starCount += parseInt(ratings[j].noofstars);
+  //         }
 
-//       }
+  //     }
 
-//       totalstarforRatingCount = totalNoRatings * 5;
-//       average = parseInt((starCount / totalstarforRatingCount) * 5);
-//       console.log(average);
-//       if(id == 1){
-//       displayStarRating(i,average);
-//       }
-//       itemWithRatings = {
-//         itemID : items[i]._id,
-//         averageRating :average
-//       }
-//       itemsWithRatings.push(itemWithRatings);
-//       console.log(itemsWithRatings);
+  //     totalstarforRatingCount = totalNoRatings * 5;
+  //     average = parseInt((starCount / totalstarforRatingCount) * 5);
+  //     console.log(average);
+  //     if(id == 1){
+  //     displayStarRating(i,average);
+  //     }
+  //     itemWithRatings = {
+  //       itemID : items[i]._id,
+  //       averageRating :average
+  //     }
+  //     itemsWithRatings.push(itemWithRatings);
+  //     console.log(itemsWithRatings);
 
-//     }
+  //   }
 
-//   }
+  // }
 
-//   function displayStarRating(id,totalAverage){
-//     let txt = "";
-//       if(isNaN(totalAverage)){
-//         txt = "No Ratings yet!";
-//         document.getElementById(id +'stars').innerHTML = txt;
-//         //document.getElementById(id +'stars').style.color = "#FF0000";
-//       }else{
+  // function displayStarRating(id,totalAverage){
+  //   let txt = "";
+  //     if(isNaN(totalAverage)){
+  //       txt = "No Ratings yet!";
+  //    //   document.getElementById(id +'stars').innerHTML = txt;
+  //       //document.getElementById(id +'stars').style.color = "#FF0000";
+  //     }else{
 
-//       for(let j = 0; j < totalAverage; j++){
-//         txt += '<span class="fa fa-star checked"></span>';
-//       }
-//       for(let j = 0; j < (5 - totalAverage); j++){
-//         txt += '<span class="fa fa-star"></span>';
-//       }
+  //     for(let j = 0; j < totalAverage; j++){
+  //       txt += '<span class="fa fa-star checked"></span>';
+  //     }
+  //     for(let j = 0; j < (5 - totalAverage); j++){
+  //       txt += '<span class="fa fa-star"></span>';
+  //     }
 
-//       document.getElementById(id +'stars').innerHTML = txt +'  '+ totalAverage + '.0 / 5.0';
-//      }
-//   }
+  //    // document.getElementById(id +'stars').innerHTML = txt +'  '+ totalAverage + '.0 / 5.0';
+  //    }
+  // }
 
-//   function displayStatus(){
-//     for(let i = 0; i < items.length; i++){
+  // function displayStatus(){
+  //   for(let i = 0; i < items.length; i++){
 
-//       if(items[i].ItemAvailabilityStatus == true){
-//         document.getElementById(i+'x').checked = true;
-//         document.getElementById(i).innerHTML = "Item Available";
-//         document.getElementById(i).style.color = "#A4DE02";
+  //     if(items[i].ItemAvailabilityStatus == true){
+  //       document.getElementById(i+'x').checked = true;
+  //       document.getElementById(i).innerHTML = "Item Available";
+  //       document.getElementById(i).style.color = "#A4DE02";
 
-//       }else if(items[i].ItemAvailabilityStatus == false){
-//         document.getElementById(i+'x').checked = false;
-//         document.getElementById(i).innerHTML = "Item Out of Stock";
-//         document.getElementById(i).style.color = "#FF0000";
-//       }
-//     }
+  //     }else if(items[i].ItemAvailabilityStatus == false){
+  //       document.getElementById(i+'x').checked = false;
+  //       document.getElementById(i).innerHTML = "Item Out of Stock";
+  //       document.getElementById(i).style.color = "#FF0000";
+  //     }
+  //   }
 
-//   }
-//   function more(num) {
-//     if (document.getElementById(num).innerHTML == "") {
-//       document.getElementById(num).innerHTML =
-//         "A paragraph is a series of related sentences developing a central idea, called the topic. Try to think about paragraphs in terms of thematic unity: a paragraph is a sentence or a group of sentences that supports one central, unified idea. Paragraphs add one idea at a time to your broader argument.";
-//       document.getElementById("showBtn").innerHTML = "Minimize";
-//     } else {
-//       document.getElementById(num).innerHTML = "";
-//       document.getElementById("showBtn").innerHTML = "show more";
-//     }
-//   }
-return(<div></div>)
-//   return (
-//     <div>
-//       <div class="row">
-//         {items.map((items, index) => {
-//           <div class="col-sm-6">
-//             <br />
-//             <div class="card">
-//               <div class="row no-gutters">
-//                 <div class="col-sm-5" style={{ background: "#ffffff" }}>
-//                   <img
-//                     src={"/Images/iphone-x-.jpg"}
-//                     class="card-img-top h-100"
-//                     style={{ width: "250px" }}
-//                     alt="..."
-//                   />
-//                 </div>
-//                 <div class="col-sm-7">
-//                   <div class="card-body">
-//                     <h5 class="card-title">Brand Alice Liddel</h5>
-//                     <div  id = {index +'stars'} class="card-text">
-//                       <span class="fa fa-star checked"></span>
-//                       <span class="fa fa-star checked"></span>
-//                       <span class="fa fa-star checked"></span>
-//                       <span class="fa fa-star checked"></span>
-//                       <span class="fa fa-star"></span>
-//                       <span> </span> <span id = {index +'review'} >4.0 / 5.0</span>
-//                     </div>
-//                     <p class="card-text">
-//                       <b>Price</b>
-//                     </p>
-//                     <p class="card-text">Item status</p>
-//                     <a href="#" class="btn btn-danger ">
-//                       Remove from list
-//                     </a>{" "}
-//                     <span> </span>
-//                     <button class="btn btn-success">Add to cart</button>{" "}
-//                     <span></span>
-//                     <br />
-//                     <br />
-//                     <span></span>
-//                     <button
-//                       id="showBtn"
-//                       class="btn btn-primary"
-//                       onClick={() => more("w1")}
-//                     >
-//                       Show More
-//                     </button>
-//                   </div>
-//                 </div>
-//                 <div id="w1"></div>
-//               </div>
-//             </div>
-//           </div>;
-//         })}
-//       </div>
-//     </div>
-//   );
+  // }
+  function more(num) {
+    if (document.getElementById(num).innerHTML == "") {
+      document.getElementById(num).innerHTML =
+        "A paragraph is a series of related sentences developing a central idea, called the topic. Try to think about paragraphs in terms of thematic unity: a paragraph is a sentence or a group of sentences that supports one central, unified idea. Paragraphs add one idea at a time to your broader argument.";
+      document.getElementById("showBtn").innerHTML = "Minimize";
+    } else {
+      document.getElementById(num).innerHTML = "";
+      document.getElementById("showBtn").innerHTML = "show more";
+    }
+  }
+  return (
+    <div>
+      <div class="row">
+        {items.map((items, index) => {
+          <div class="col-sm-6">
+            <br />
+            <div class="card">
+              <div class="row no-gutters">
+                <div class="col-sm-5" style={{ background: "#ffffff" }}>
+                  <img
+                    src={"/Images/iphone-x-.jpg"}
+                    class="card-img-top h-100"
+                    style={{ width: "250px" }}
+                    alt="..."
+                  />
+                </div>
+                <div class="col-sm-7">
+                  <div class="card-body">
+                    <h5 class="card-title">Brand Alice Liddel</h5>
+                    <div  id = {index +'stars'} class="card-text">
+                      <span class="fa fa-star checked"></span>
+                      <span class="fa fa-star checked"></span>
+                      <span class="fa fa-star checked"></span>
+                      <span class="fa fa-star checked"></span>
+                      <span class="fa fa-star"></span>
+                      <span> </span> <span id = {index +'review'} >4.0 / 5.0</span>
+                    </div>
+                    <p class="card-text">
+                      <b>Price</b>
+                    </p>
+                    <p class="card-text">Item status</p>
+                    <a href="#" class="btn btn-danger ">
+                      Remove from list
+                    </a>{" "}
+                    <span> </span>
+                    <button class="btn btn-success">Add to cart</button>{" "}
+                    <span></span>
+                    <br />
+                    <br />
+                    <span></span>
+                    <button
+                      id="showBtn"
+                      class="btn btn-primary"
+                      onClick={() => more("w1")}
+                    >
+                      Show More
+                    </button>
+                  </div>
+                </div>
+                <div id="w1"></div>
+              </div>
+            </div>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
 }
