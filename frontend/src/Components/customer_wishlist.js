f1f44247945f59aeabf2d0e3f5639b5db3e610b1
sonal-uchiru <sonal123326@gmail.com>
2021-10-01 09:46:58 +0530

    wishlist updated

--- frontend/src/Components/customer_wishlist.js
+++ frontend/src/Components/customer_wishlist.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,radix } from "react";
 import { ClipLoader } from "react-spinners";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
