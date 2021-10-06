05fde6e0d33fbbecbdb9e1cae64b4569ada527a1
Kaveen1025 <IT20185770@my.sliit.lk>
2021-10-06 16:46:13 +0530

    dashboard

--- frontend/src/Components/AdminDashBoard.js
+++ frontend/src/Components/AdminDashBoard.js
@@ -123,7 +123,7 @@ export default function AdminDashBoard(props) {
 
 <div class="row">
   <div class="column">
-    <div class="card" style={{backgroundColor:"#fefefe", backgroundImage: "linear-gradient(315deg, #fefefe 0%, #00a4e4 74%)"}}>
+    <div class="card" style={{backgroundColor:"rgb(36,37,255)", background: "linear-gradient(90deg, rgba(36,37,255,1) 0%, rgba(255,255,255,1) 100%)"}}>
       
     <img src="https://img.icons8.com/fluency/300/000000/group.png" style={{width:"120px", height:"120px", marginLeft:"75px"}}/>
       <h3>Total Customers</h3>
@@ -132,7 +132,7 @@ export default function AdminDashBoard(props) {
   </div>
 
   <div class="column">
-    <div class="card" style={{backgroundColor:"#e96196", backgroundImage: "linear-gradient(315deg, #e96196 0%, #ffffff 74%)"}}>
+    <div class="card"  style={{backgroundColor:" rgb(255,173,110)", background: "linear-gradient(90deg, rgba(255,173,110,1) 3%, rgba(255,255,255,1) 100%)"}}>
     
     <img src="https://img.icons8.com/fluency/300/000000/guest-male.png" style={{width:"120px", height:"120px", marginLeft:"75px"}}/>
       <h3>Total Sellers</h3>
@@ -142,7 +142,7 @@ export default function AdminDashBoard(props) {
   </div>
   
   <div class="column">
-    <div class="card" style={{backgroundColor: "rgb(2,0,36)", background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(255,255,255,1) 0%, rgba(0,212,255,1) 86%)"}}>
+    <div class="card" style={{backgroundColor: "rgb(255,235,76)", background: "linear-gradient(90deg, rgba(255,235,76,1) 3%, rgba(255,255,255,1) 100%)"}}>
       
     <img src="https://img.icons8.com/fluency/300/000000/multiple-smartphones.png" style={{width:"120px", height:"120px", marginLeft:"60px"}}/>
       <h3>Total Items</h3>
@@ -152,7 +152,7 @@ export default function AdminDashBoard(props) {
   </div>
   
   <div class="column">
-    <div class="card" style={{backgroundColor:"#fffcf9", backgroundImage: "linear-gradient(315deg, #fffcf9 0%, #ffd166 74%)"}}>
+    <div class="card"  style={{backgroundColor:" rgb(255,103,200)", background: "linear-gradient(90deg, rgba(255,103,200,1) 3%, rgba(255,255,255,1) 100%)"}}>
      
     <img src="https://img.icons8.com/fluency/300/000000/package.png" style={{width:"120px", height:"120px", marginLeft:"75px"}}/>
       <h3>Total Packages</h3>
@@ -166,7 +166,7 @@ export default function AdminDashBoard(props) {
 
 <div class="row">
   <div class="column">
-    <div class="card" style={{backgroundColor: "#ffffff", backgroundImage: "linear-gradient(315deg, #ffffff 0%, #cea177 74%)"}}>
+    <div class="card" style={{backgroundColor: "rgb(255,98,98)", background: "linear-gradient(90deg, rgba(255,98,98,1) 3%, rgba(255,255,255,1) 100%)"}}>
     <img src="https://img.icons8.com/fluency/300/000000/administrator-male.png" style={{width:"120px", height:"120px", marginLeft:"75px"}}/>
       <h3>Total Admins</h3>
       <p><b>{adminCount}</b></p>
@@ -175,7 +175,7 @@ export default function AdminDashBoard(props) {
   </div>
 
   <div class="column">
-    <div class="card" style={{backgroundColor: "#5ca0f2", backgroundImage: "linear-gradient(315deg, #5ca0f2 0%, #f5f7f6 74%)"}}>
+    <div class="card" style={{backgroundColor: "rgb(180,68,91)", background: "linear-gradient(90deg, rgba(180,68,91,1) 3%, rgba(255,255,255,1) 100%)"}}>
     <img src="https://img.icons8.com/fluency/300/000000/invite.png"  style={{width:"120px", height:"120px", marginLeft:"75px"}}/>
       <h3>Seller Requests</h3>
       <p><b>{sellerRequesCount}</b></p>
