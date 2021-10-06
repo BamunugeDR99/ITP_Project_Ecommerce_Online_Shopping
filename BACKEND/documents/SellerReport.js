75c357d8012afe65035621e8f87a046d54244625
Heshishivani <heshishivani@gmail.com>
2021-10-07 03:23:44 +0530

    SellerReport - Template

---
+++ BACKEND/documents/SellerReport.js
@@ -0,0 +1,185 @@
+module.exports = (objarr) => {
+
+  
+
+    return `
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+         .header{
+             font-family: Comic Sans MS, Comic Sans, cursive;
+             font-size: 40px;
+             margin-top: -20px;
+         }
+    
+         .headerCard{
+             text-align: center;
+             margin-top: -30px;
+         }
+         .header2{
+            font-family: Comic Sans MS, Comic Sans, cursive;
+            margin-top: -20px;
+            font-size: 20px;
+            
+         }
+    
+         .mainFooter{
+            text-align: right;
+         }
+
+         table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 85%;
+          }
+          .caltext {
+            padding-left : 110px;
+          }
+          
+          th {
+            border: 1px solid #000000;
+            text-align: center;
+            padding: 20px;
+            background-color: #ffffff;
+          }
+          td {
+            border: 1px solid #000000;
+            text-align: center;
+            padding: 15px;
+          }
+          
+          tr:nth-child(even) {
+            background-color: #bfbfbf;
+          }
+          
+          tr:nth-child(odd) {
+            background-color: #f2f2f2;
+          }
+         
+    
+        </style>
+    </head>
+    <header>
+        <div class = "headerCard">
+            <img src="https://cloud9.gg/wp-content/uploads/2020/07/Blue_Valorant_144.png"  class = "logo" alt="TechScope logo">
+            <h1 class="header">TechScope</h1></img>
+            <h3 class = "header2">See what's selling best</h3>
+        </div>
+      </header>
+    <body>
+     
+    <br></br>
+<table align="center">
+  <tr>
+    <th>DATE</th>
+    <th>COMPANY NAME</th>
+    <th>OWNER NAME</th>
+	<th>YEAR</th>
+    <th>EMAIL</th>
+	<th>CONTACT NUMBER</th>
+    <th>USERNAME</th>
+	<th>MOST ACTIVE DAYS</th>
+  </tr>
+  
+  <tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr>
+  <tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr>
+  <tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr>
+  <tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr>
+  <tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr><tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr>
+  <tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr>
+  <tr>
+    <td>06/09/2021</td>
+    <td>Dialcom</td>
+    <td>Dasun Silva</td>
+	<td>2014</td>
+    <td>dds123@gmail.com</td>
+	<td>0772113227</td>
+	<td>Ddsil12</td>
+	<td>06</td>
+  </tr>
+
+
+</table>
+<br/>
+<h3 class = "caltext">Total number of sellers accepted on this month : </h3>
+
+<h3 class = "caltext">Maximum number of sellers were accepted on : </h3>
+<br/>
+         
+     
+    </body>
+    <footer>
+        <hr>
+        <h4 class = "mainFooter">2021 &copy;TechScope Ltd.</h4>
+    </footer>
+    </html>
+      `;
+  };
+  
